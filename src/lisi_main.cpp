fa9a8115233a8379ecacec23921f2415df05c332
Niels Hygum Nielsen <you@example.com>
2025-08-24 17:03:53 +0200

    wip

--- src/lisi_main.cpp
+++ src/lisi_main.cpp
@@ -22,10 +22,14 @@ int main() {
                pulse = 460;
             if (input == 's')
                 pulse -= 5;*/
+            std::cout << "set channel" << std::endl;
             std::cin >> input;
+            int channel = std::stoi(input);
+            std::cout << "set angle" << std::endl;
+            float angle = std::stof(input);
             std::cout << "input was: " << input << "\n";
-       // i2c_pwm_controller.setServoAngle(13, input) //steering
-        i2c_pwm_controller.setServoAngle(15, std::stof(input)) //throttle
+       // i2c_pwm_controller.setServoAngle(13, input) //steering //channel 13 steering channel 15 throttle
+        i2c_pwm_controller.setServoAngle(channel, std::stof(input)); //throttle
     }
 
     return 0;
